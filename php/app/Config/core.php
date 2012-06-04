a78553fb16dee6d9fea005ba64e0d637e685e1eb
Chris Stevens <chris.stevens@opentaxi.com>
2012-06-03 21:20:16 -0500

    Changed default Security.sale and Security.cipherSeed

--- php/app/Config/core.php
+++ php/app/Config/core.php
@@ -184,12 +184,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'hFFMrGFNtyf93pMzIMcnvzfI5zhCgWAZvZgkzZLI');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '71197317043113014946276614821701');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
